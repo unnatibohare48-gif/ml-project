35fe6484c887eeb13717f04e69c44f0d1ef608be
unnati bohare <palakbohare48@gmail.com>
2026-09-22 11:30:46 +0530

    data_ingestion

--- src/exeption.py
+++ src/exeption.py
@@ -1,7 +1,7 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_details(error,error_details:sys):
+def error_message_details(error, error_details):
     _,_,exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = 'error occured in python script [{0}] line number [{1}] error message[{2}]'.format(
@@ -9,18 +9,9 @@ def error_message_details(error,error_details:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_details:sys):
+    def __init__(self, error_message, error_details):
         super().__init__(error_message)
-        self.error_message = error_message_details(error_message, error_details, error_details=error_details)
+        self.error_message = error_message_details(error_message, error_details)
 
     def __str__(self):
         return self.error_message
-
-if __name__ == '__main__':
-
-    try:
-        a = 1/0
-    except Exception as e:
-
-        logging.info('Divide By Zero')
-        raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -1,16 +1,18 @@
-# every exeption that happens we should be able to log all those information the execution in
-# some files so that we will be able to track if there is any error even the custom exeption
+# every exception that happens we should be able to log all that information in
+# some files so that we will be able to track if there is any error even the custom exception
 # error we will try to log that in the text file
 import logging
 import os
 from datetime import datetime
-LOG_FILE = f'{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log'
-logs_path  = os.path.join(os.getcwd(),'logs',LOG_FILE)
-os.makedirs(logs_path, exist_ok=True)
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+LOG_DIR = os.path.join(os.getcwd(), 'logs')
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
 logging.basicConfig(
-    filename = LOG_FILE_PATH,
-    format = '[%(asctime)s] %(lineno)d - %(name)s - %(message)s',
-    level = logging.INFO,
-    
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(lineno)d - %(name)s - %(message)s',
+    level=logging.INFO,
 )
